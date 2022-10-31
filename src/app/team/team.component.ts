ca88f4c57078b3550d3f869c2e6156b16a3509ae
Santhosh <santhoshvenkateswaran.s@gmail.com>
2022-10-31 20:57:56 +0530

    replace tslint with eslint

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.scss']
+  styleUrls: ['./team.component.scss'],
 })
-export class TeamComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TeamComponent {}
