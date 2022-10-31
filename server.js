328b18b9edd40dfe60497ed3a6074216540c4f71
Santhosh <santhoshvenkateswaran.s@gmail.com>
2022-10-31 21:43:21 +0530

    add server.js for heroku deployments

---
+++ server.js
@@ -0,0 +1,15 @@
+// Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve only the static files form the angular app directory
+app.use(express.static(__dirname + "/dist/major-project"));
+
+app.get("/*", function (_req, res) {
+  res.sendFile(path.join(__dirname + "/dist/major-project/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
